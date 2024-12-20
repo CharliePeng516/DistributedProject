2e36d986ffb64223a2cfc9c69af2185be46a920c
CharliePeng <charliepeng516@gmail.com>
2024-12-20 20:01:54 +1100

    While practice

---
+++ Java_Practice/src/practice/WhilePractice.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class WhilePractice {
+    public static void main(String[] args) {
+        int sum = 0;
+        int m = 20;
+        int n = 100;
+        // 使用while计算M+...+N:
+        while (m <= n) {
+            sum += m;
+            m ++;
+        }
+        System.out.println(sum);
+    }
+}
