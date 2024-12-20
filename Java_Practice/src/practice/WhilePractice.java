dc1c8931bb3dba8a4c0be59ae53109de406cfbd5
CharliePeng <charliepeng516@gmail.com>
2024-12-20 20:04:17 +1100

    do while practice

--- Java_Practice/src/practice/WhilePractice.java
+++ Java_Practice/src/practice/WhilePractice.java
@@ -2,6 +2,11 @@ package practice;
 
 public class WhilePractice {
     public static void main(String[] args) {
+        WhileP();
+        DoWhilePractice();
+
+    }
+    public static void WhileP() {
         int sum = 0;
         int m = 20;
         int n = 100;
@@ -10,6 +15,20 @@ public class WhilePractice {
             sum += m;
             m ++;
         }
+        System.out.println(sum);
+    }
+
+
+
+    public static void DoWhilePractice() {
+        int sum = 0;
+        int m = 20;
+        int n = 100;
+        // 使用do while计算M+...+N:
+        do {
+            sum += m;
+            m ++;
+        } while (m<=n);
         System.out.println(sum);
     }
 }
